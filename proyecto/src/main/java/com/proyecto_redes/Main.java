d6a371c6ac97ce4089c1357bef9f992c066fee11
TruenoPai89 <carloscondor3399@gmail.com>
2026-03-22 00:59:33 +0100

    comit inical

---
+++ proyecto/src/main/java/com/proyecto_redes/Main.java
@@ -0,0 +1,7 @@
+package com.proyecto_redes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello wo");
+    }
+}
---
+++ proyecto/src/main/java/com/proyecto_redes/model/Product.java
@@ -0,0 +1,13 @@
+package com.proyecto_redes.model;
+
+public class Product {
+    private String name;
+    private String brand;
+    private String category;
+
+    public Product(){
+        this.name=null;
+        this.brand=null;
+        this.category=null;
+    }
+}
